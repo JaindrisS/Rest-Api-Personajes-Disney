29ac6af503ec04d3ba69168cdc191dfe884f2912
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-21 20:56:51 -0500

    add registerUser

--- userCase/userControllers.js
+++ userCase/userControllers.js
@@ -1,7 +1,9 @@
 const obtener = require("./getUser/getUser");
 const acceso = require("./loginUSer/loginUser");
+const registrarUsuarios = require("./registerUser/registerUser");
 
 module.exports = {
   obtener,
   acceso,
+  registrarUsuarios,
 };
