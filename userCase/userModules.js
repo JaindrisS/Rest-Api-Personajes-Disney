1f92da7150f2ad4d56ccc9562a6744e2b5dc024b
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-21 20:23:45 -0500

    add userCase userModules

---
+++ userCase/userModules.js
@@ -0,0 +1,14 @@
+const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
+const Usuarios = require("../models/usuario");
+const { generarJWT } = require("../helpers/generarJWT");
+const { sendMail } = require("../helpers/emailer");
+
+module.exports = {
+  response,
+  request,
+  bcryptjs,
+  Usuarios,
+  generarJWT,
+  sendMail,
+};
