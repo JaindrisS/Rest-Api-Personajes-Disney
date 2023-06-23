71f1f17a22704fc8e4aea6de5387252877f302f3
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-22 20:18:38 -0500

    add general search

---
+++ useCases/search/generalSearch/search.js
@@ -0,0 +1,27 @@
+
+const {buscarPersonaje,buscarPeliculas}=require("../searchControllers")
+
+const buscar = (req = request, res = response) => {
+  const { coleccion, termino } = req.params;
+
+  if (!coleccionesPermitidas.includes(coleccion)) {
+    return res.status(400).json({
+      msg: `Las colecciones permitidas son ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case "peliculas":
+   buscarPeliculas(termino, res, req);
+      break;
+    case "personajes":
+      buscarPersonaje(termino, res);
+      break;
+    default:
+      res.status(500).json({
+        msg: " busqueda no agregada",
+      });
+      break;
+  }
+};
+module.exports = buscar;
