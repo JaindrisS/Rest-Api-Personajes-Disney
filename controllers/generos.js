9ca25b9783400d90a3514130bc99a8b06a5f308b
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-22 21:35:24 -0500

    delete controller obtener genero

--- controllers/generos.js
+++ controllers/generos.js
@@ -2,33 +2,10 @@ const { response, request } = require("express");
 
 const Genero = require("../models/genero");
 
-const obtenerGenero = async (req = request, res = response) => {
-  const genero = await Genero.aggregate([
-    {
-      $lookup: {
-        from: "peliculas", //2
-        localField: "_id", //referencia al id gel genero, 1 (pelicula)
-        foreignField: "genero", //id coincida con el campo de pelicula 2
-        as: "peliculaasociada",
-      },
-    },
-    {
-      $project: {
-        nombre: 1,
-        peliculaasociada: { titulo: 1, imagen: 1 },
-      },
-    },
-  ]);
-
-  res.json(genero);
-};
-
 const crearGenero = async (req = request, res = response) => {
   const { ...body } = req.body;
 
   const nombre = body.nombre.toUpperCase();
-
-  
 
   const datos = {
     ...body,
