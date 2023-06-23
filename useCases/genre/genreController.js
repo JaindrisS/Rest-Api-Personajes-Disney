23caa8e86447bbffa0c61598d5df2d1b202c7b3a
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-22 21:47:35 -0500

    add controller actualizar genero

--- useCases/genre/genreController.js
+++ useCases/genre/genreController.js
@@ -1,4 +1,5 @@
 const obtenerGenero = require("../genre/getGenre/getGenre");
 const crearGenero = require("../genre/createGenre/createGenre");
+const actualizarGenero = require("../genre/updateGenre/updateGenre")
 
-module.exports = { obtenerGenero, crearGenero };
+module.exports = { obtenerGenero, crearGenero,actualizarGenero };
