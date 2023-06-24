4357fd2ea692219038d8255bff74003197a3f40f
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-23 21:24:27 -0500

    delete controller actualizar personaje

--- controllers/personajes.js
+++ controllers/personajes.js
@@ -1,26 +1,6 @@
 const { response, request } = require("express");
 const Personaje = require("../models/personaje");
 
-const actualizarPersonaje = async (req = request, res = response) => {
-  const { id } = req.params;
-  const { _id, estado, upDate, ...body } = req.body;
-
-  if (body.nombre) {
-    body.nombre = body.nombre.toUpperCase();
-  }
-
-  let dateUp = new Date();
-  let resultado = dateUp.toLocaleString();
-  body.upDate = resultado;
-
-  const datos = {
-    ...body,
-  };
-  const personaje = await Personaje.findByIdAndUpdate(id, datos);
-
-  res.json(personaje);
-};
-
 const borrarPersonaje = async (req = request, res = response) => {
   const { id } = req.params;
   const personaje = await Personaje.findByIdAndUpdate(id, {
@@ -32,6 +12,5 @@ const borrarPersonaje = async (req = request, res = response) => {
 };
 
 module.exports = {
-  actualizarPersonaje,
   borrarPersonaje,
 };
