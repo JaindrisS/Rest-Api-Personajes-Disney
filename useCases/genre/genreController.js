7a9f94962409a84ade87e12ecc1a10efd8c2b968
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-23 20:14:41 -0500

    add controller borrar genero

--- useCases/genre/genreController.js
+++ useCases/genre/genreController.js
@@ -1,5 +1,6 @@
 const obtenerGenero = require("../genre/getGenre/getGenre");
 const crearGenero = require("../genre/createGenre/createGenre");
-const actualizarGenero = require("../genre/updateGenre/updateGenre")
+const actualizarGenero = require("../genre/updateGenre/updateGenre");
+const borrarGenero = require("./deleteGenre/deleteGenre");
 
-module.exports = { obtenerGenero, crearGenero,actualizarGenero };
+module.exports = { obtenerGenero, crearGenero, actualizarGenero, borrarGenero };
