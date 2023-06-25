b3a924ab3e5b793dc633909d1fc66d83c2b0d71c
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-24 21:36:27 -0500

    add delete movie controller

--- useCases/movies/moviesControllers.js
+++ useCases/movies/moviesControllers.js
@@ -1,5 +1,11 @@
 const obtenerPeliculas = require("./getMovies/getMovies");
 const crearPelicula = require("./createMovies/createMovies");
 const actualizarPelicula = require("./updateMovies/updateMovie");
+const borrarPelicula = require("./deleteMovies/deleteMovie");
 
-module.exports = { obtenerPeliculas, crearPelicula, actualizarPelicula };
+module.exports = {
+  obtenerPeliculas,
+  crearPelicula,
+  actualizarPelicula,
+  borrarPelicula,
+};
