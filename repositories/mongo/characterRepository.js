3128c9443e06b3e0308b0ff5085541bafb89cab5
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-26 21:47:28 -0500

    add create

--- repositories/mongo/characterRepository.js
+++ repositories/mongo/characterRepository.js
@@ -1,15 +1,30 @@
 const Personaje = require("../../models/personaje");
 
 const getAll = async () => {
-
   const response = await Personaje.find(
     { estado: true },
     { nombre: 1 }
   ).populate("peliculaoserie", "titulo");
 
-  return response
-
+  return response;
 };
 
+const create = async (data,nombre) => {
+  const info = {
+    ...data,
+    nombre: nombre.toUpperCase(),
+  };
 
-module.exports = getAll
+  const personaje = await new Personaje(info);
+
+  let date = new Date();
+  let result = date.toLocaleString();
+  personaje.createAt = result;
+
+  // Guardar en BD
+  await personaje.save();
+
+  return personaje;
+};
+
+module.exports = { getAll, create };
