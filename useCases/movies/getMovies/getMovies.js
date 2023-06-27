f1ad26506063f1c11d150e291b3b09f3744f02a9
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-26 20:19:51 -0500

    add get all from user repository

--- useCases/movies/getMovies/getMovies.js
+++ useCases/movies/getMovies/getMovies.js
@@ -1,10 +1,8 @@
-const { request, response, Pelicula } = require("../moviesModules");
+const { request, response } = require("../moviesModules");
+const { getAll } = require("../../../repositories/mongo/moviesRepository");
 
 const obtenerPeliculas = async (req = request, res = response) => {
-  const pelicula = await Pelicula.find(
-    { estado: true },
-    { titulo: 1, imagen: 1, fechadecreacion: 1 }
-  );
+  const pelicula = await getAll();
 
   return res.status(201).json({ pelicula });
 };
