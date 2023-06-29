ee4fa26469a1b75209b4671eb7fbfae6e6ed4c2b
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-28 20:05:47 -0500

    add getMovie fron search repository

--- useCases/search/searchMovie/searchMovie.js
+++ useCases/search/searchMovie/searchMovie.js
@@ -1,49 +1,10 @@
-const { response, request, ObjectId, Pelicula } = require("../searchModules");
+const getMovie = require("../../../repositories/mongo/searchRepository");
+const { response, request } = require("../searchModules");
 
 const buscarPeliculas = async (termino = "", res = response, req = request) => {
-  const esMongoId = ObjectId.isValid(termino);
+  const response = await getMovie(termino);
 
-  if (esMongoId) {
-    const pelicula = await Pelicula.findById(termino).populate(
-      "genero",
-      "nombre"
-    );
-    return res.json({
-      Results:
-        pelicula && pelicula.estado
-          ? [pelicula]
-          : { msg: "La pelicula no existe" },
-    });
-  }
-
-  // busqueda por parametros ASC | DES
-  if (termino) {
-    if (termino === "asc") {
-      const pelicula = await Pelicula.find().sort({ fechadecreacion: 1 });
-
-      return res.json({
-        Peliculas: pelicula,
-      });
-    }
-    if (termino === "desc") {
-      const pelicula = await Pelicula.find().sort({ fechadecreacion: -1 });
-
-      return res.json({
-        Peliculas: pelicula,
-      });
-    }
-  }
-
-  // Busqueda por nombre
-  const regex = new RegExp(termino, "i");
-
-  const pelicula = await Pelicula.find({
-    $and: [{ titulo: regex }, { estado: true }],
-  });
-
-  return res.json({
-    Results: pelicula ? pelicula : { msg: `La pelicula ${termino} no existe` },
-  });
+  return res.status(response.status).json(response.msg);
 };
 
 module.exports = buscarPeliculas;
