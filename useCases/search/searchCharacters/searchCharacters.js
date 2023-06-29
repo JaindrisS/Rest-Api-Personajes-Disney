9f6ce90d9ec32fb6f9916e996f53da293e044cab
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-06-28 21:08:59 -0500

    add getCharacter from character repository

--- useCases/search/searchCharacters/searchCharacters.js
+++ useCases/search/searchCharacters/searchCharacters.js
@@ -1,32 +1,12 @@
-const { response, ObjectId, Personaje } = require("../searchModules");
+const {
+  getCharacter,
+} = require("../../../repositories/mongo/searchRepository");
+const { response } = require("../searchModules");
 
 const buscarPersonaje = async (termino = "", res = response) => {
-  // busqueda por id de mongodb
-  const esMongoId = ObjectId.isValid(termino);
-
-  if (esMongoId) {
-    const personaje = await Personaje.findById(termino);
-
-    res.json({
-      results:
-        personaje && personaje.estado
-          ? [personaje]
-          : { msg: `El id del personaje ${termino} no existe o no es valido` },
-    });
-  }
-
-  // buscar por nombre
-
-  const regex = new RegExp(termino, "i");
-
-  const personaje = await Personaje.find({
-    $and: [{ nombre: regex }, { estado: true }],
-  }).populate("peliculaoserie", "titulo");
-  return res.json({
-    results: personaje
-      ? personaje
-      : { msg: `El nombre del personaje ${termino} no existe` },
-  });
+  const response = await getCharacter(termino);
+  console.log(response.status);
+  return res.status(response.status).json(response.msg);
 };
 
 module.exports = buscarPersonaje;
